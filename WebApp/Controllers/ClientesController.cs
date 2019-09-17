4d037a25a89c640f4370802d376fa36d17de6cce
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2019-09-17 19:13:09 -0300

    Rodrigo

---
+++ WebApp/Controllers/ClientesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class ClientesController : Controller
+    {
+        public IActionResult Listar()
+        {
+            List<Cliente> clientes = new List<Cliente>();
+            clientes.Add(new Cliente { id = 1, nome_cliente = "Rodrigo Luiz", celular_cliente = "11995882409" });
+            clientes.Add(new Cliente { id = 2, nome_cliente = "José Luiz", celular_cliente = "11995882499" });
+            return View(clientes);
+        }
+        public IActionResult Adicionar()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApp/Models/Cliente.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApp.Models
+{
+    public class Cliente
+    {
+        public int id { get; set; }
+        public string nome_cliente { get; set; }
+        public string celular_cliente { get; set; }
+    }
+}
